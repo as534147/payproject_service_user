baa5b15e097f0732c0c271668309a2d0ae6cf8bd
sofire_glo <237175189@qq.com>
2020-06-19 22:26:18 +0800

    项目提交

---
+++ src/main/java/com/llj/sofire/service/user/dao/PmsUserDao.java
@@ -0,0 +1,27 @@
+package com.llj.sofire.service.user.dao;
+
+import com.llj.sofire.service.user.common.core.dao.BaseDao;
+import com.llj.sofire.user.entity.PmsUser;
+
+
+
+
+/**
+ * 
+ * @描述: 用户表数据访问层接口.
+ * @作者: WuShuicheng .
+ * @创建时间: 2013-7-22,下午5:51:47 .
+ * @版本: 1.0 .
+ */
+public interface PmsUserDao extends BaseDao<PmsUser> {
+
+	/**
+	 * 根据用户登录名获取用户信息.
+	 * 
+	 * @param loginName
+	 *            .
+	 * @return user .
+	 */
+	PmsUser findByUserNo(String userNo);
+
+}
